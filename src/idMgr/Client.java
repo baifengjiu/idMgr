66f879fb30060ea03e194c0140b4ba4d5d629646
baifengjiu <baifengjiu@outlook.com>
2017-01-23 11:43:33 +0800

    delete sql

--- src/idMgr/Client.java
+++ src/idMgr/Client.java
@@ -29,41 +29,34 @@ public class Client {
 	static Lock lock = new ReentrantLock();
 	
 	//设置id
-	public static String setID(ArrayList<?> sqls){
-		String sql;
-		String data;
-		ArrayList<String> loadsql = new ArrayList<String>();
+	public static long getID(){
 		try {
 			File config= new File(path);
 			SAXReader reader = new SAXReader();		
 			Doc = reader.read(config);
-			
-			for(int i=0;i<sqls.size();i++){
-				
-				if(flag){
-					//init
-					cur_id=readXML();					
-					flag=false;
-				}else{
-					cur_id=getNewId();
-				}
-				
-				if(cur_id == -1){
-					System.out.println("没有获取到id块");
-					return "error";
-				}
-				//add id and sql to sql file  
-				sql=sqls.get(i).toString();
-
-				data = "insert into idMgr values("+cur_id+",\""+ sql +"\");";
-				loadsql.add(data);
+			if(flag){
+				//init
+				Element root = Doc.getRootElement();				
+				cur_id = Integer.parseInt(root.elementText("start"));
+				num = Integer.parseInt(root.elementText("num"));
+				//System.out.println("当前起始id:"+cur_id);
+				//get the new id block form server
+				cur_id=getNewBlock(num);					
+				flag=false;
+			}else{
+				cur_id=getNewId();
 			}
-			LoadSql.setSql(loadsql);	
+			
+			if(cur_id == -1){
+				System.out.println("没有获取到id块");
+				return -1;
+			}
+			
 		} catch (Exception e) {
 			// TODO 自动生成的 catch 块
 			e.printStackTrace();
 		}
-		return "ok";
+		return cur_id;
 	}
 	
 	private static long getNewId() {
@@ -74,11 +67,11 @@ public class Client {
 		//判断是否越界
 		if( cur_id >= edge)
 		{
+			//get the new id block form server
 			cur_id = getNewBlock(num);
 			edge=cur_id + num;
 		}
-		rstl = ++cur_id;
-		//System.out.println("cur_id:"+ cur_id +"   edge:"+ edge);		
+		rstl = ++cur_id;		
 		lock.unlock();
 		return rstl;
 	}
@@ -86,7 +79,7 @@ public class Client {
 	//get id block
 	private static long getNewBlock(long size) {		
 		// TODO 自动生成的方法存根
-		System.out.println("++++正在分配id...+++++");
+		System.out.println("++++请求服务器分配id+++++");
 		long rslt=-1;
 		Socket socket = null; 
 		try {
@@ -120,7 +113,7 @@ public class Client {
 				}  
 			}  
 		} 
- 		System.out.println("+++++id分配完毕...+++++");
+ 		System.out.println("+++++请求服务器id分配完毕+++++");
  		try {
 			Element root = Doc.getRootElement();
 			
@@ -133,21 +126,5 @@ public class Client {
 			e.printStackTrace();
 		}
 		return rslt;		
-	}
-	
-	//read config.xml file,get the start id
-	public static long readXML(){
-		long start;
-
-		Element root = Doc.getRootElement();
-		
-		start = Integer.parseInt(root.elementText("start"));
-		num = Integer.parseInt(root.elementText("num"));
-
-		System.out.println("起始id:"+start);
-		
-		//init, get the id block
-		start=getNewBlock(num);
-		return start;
 	}
 }
--- src/idMgr/Test.java
+++ src/idMgr/Test.java
@@ -6,24 +6,10 @@ public class Test {
 
 	public static void main(String[] args) {
 		// TODO 自动生成的方法存根
-		ArrayList<String> sqls = new ArrayList();
-		sqls.add("insert into t_jhk values('2','5','0','503','01','020302','金融工程');");
-		sqls.add("insert into t_jhk values('2','5','0','503','01','020302','金融工程');");
-		sqls.add("insert into t_jhk values('2','5','0','503','01','020302','金融工程');");
-		sqls.add("insert into t_jhk values('2','5','0','503','01','020302','金融工程');");
-		sqls.add("insert into t_jhk values('2','5','0','503','01','020302','金融工程');");
-		sqls.add("insert into t_jhk values('2','5','0','503','01','020302','金融工程');");
-		sqls.add("insert into t_jhk values('2','5','0','503','01','020302','金融工程');");
-		sqls.add("insert into t_jhk values('2','5','0','503','01','020302','金融工程');");
-		sqls.add("insert into t_jhk values('2','5','0','503','01','020302','金融工程');");
-		sqls.add("insert into t_jhk values('2','5','0','503','01','020302','金融工程');");
-		sqls.add("insert into t_jhk values('2','5','0','503','01','020302','金融工程');");
-		sqls.add("insert into t_jhk values('2','5','0','503','01','020302','金融工程');");
-		sqls.add("insert into t_jhk values('2','5','0','503','01','020302','金融工程');");
-		sqls.add("insert into t_jhk values('2','5','0','503','01','020302','金融工程');");
-		sqls.add("insert into t_jhk values('2','5','0','503','01','020302','金融工程');");
-		sqls.add("insert into t_jhk values('2','5','0','503','01','020302','金融工程');");
-		Client.setID(sqls);
+		System.out.println("id："+Client.getID());
+		System.out.println("id："+Client.getID());
+		System.out.println("id："+Client.getID());
+		System.out.println("id："+Client.getID());
 	}
 
 }
