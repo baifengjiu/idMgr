607e8e2c3225cb4a73ab6bd50c2d5e3d4fc76302
Clara <baifengjiu@outlook.com>
2017-03-22 12:00:06 +0800

    最终版

--- src/idMgr/Client.java
+++ src/idMgr/Client.java
@@ -127,4 +127,13 @@ public class Client {
 		}
 		return rslt;		
 	}
+	
+	
+	public static void main(String[] args) {
+		// TODO 自动生成的方法存根
+		System.out.println("id："+Client.getID());
+		System.out.println("id："+Client.getID());
+		System.out.println("id："+Client.getID());
+		System.out.println("id："+Client.getID());
+	}
 }
